1a77c8061d50d4bf6be1c71bca547a6affbfe353
roman <romanbromidge@gmail.com>
2019-03-01 18:36:26 +0000

    added line interpolation

--- Source/skeleton.cpp
+++ Source/skeleton.cpp
@@ -7,6 +7,7 @@
 
 using namespace std;
 using glm::ivec2;
+using glm::vec2;
 using glm::vec3;
 using glm::mat3;
 using glm::vec4;
@@ -30,6 +31,9 @@ bool Update();
 void Draw(screen* screen);
 void VertexShader( const vec4& v, ivec2& p );
 void TransformationMatrix(mat4 Tranformation, vec3 angles, vec3 translation);
+void Interpolate( ivec2 a, ivec2 b, vector<ivec2>& result );
+void DrawLineSDL( screen* screen, ivec2 a, ivec2 b, vec3 color );
+void DrawPolygonEdges( const vector<vec4>& vertices, screen* screen );
 
 
 int main( int argc, char* argv[] )
@@ -61,20 +65,15 @@ void Draw(screen* screen)
     vertices[1] = triangles[i].v1;
     vertices[2] = triangles[i].v2;
 
-    for(int v=0; v<3; ++v){
-      ivec2 projPos;
-      VertexShader( vertices[v], projPos );
-      vec3 color(1,1,1);
-      PutPixelSDL( screen, projPos.x, projPos.y, color );
-    }
-  }
+    // for(int v=0; v<3; ++v){
+    //   ivec2 projPos;
+    //   VertexShader( vertices[v], projPos );
+    //   vec3 color(1,1,1);
+    //   PutPixelSDL( screen, projPos.x, projPos.y, color );
+    // }
 
-  // vec3 colour(1.0,0.0,0.0);
-  // for(int i=0; i<1000; i++){
-  //     uint32_t x = rand() % screen->width;
-  //     uint32_t y = rand() % screen->height;
-  //     PutPixelSDL(screen, x, y, colour);
-  // }
+    DrawPolygonEdges(vertices, screen);
+  }
 }
 
 /*Place updates of parameters here*/
@@ -120,13 +119,56 @@ bool Update()
   return true;
 }
 
+//Interpolate a 2d vector between two 2d points
+void Interpolate( ivec2 a, ivec2 b, vector<ivec2>& result ) {
+  int N = result.size();
+  vec2 step = vec2(b-a) / float(max(N-1,1));
+  vec2 current( a );
+  for( int i=0; i<N; ++i ) {
+    result[i] = current;
+    current += step;
+  }
+}
+
+//Draw a line between 2 points on the screen
+void DrawLineSDL( screen* screen, ivec2 a, ivec2 b, vec3 color ) {
+  //Find the absolute difference
+  ivec2 delta = glm::abs( a - b );
+  int pixels = glm::max( delta.x, delta.y ) + 1;
+  //Create a vector of vectors
+  vector<ivec2> line( pixels );
+  Interpolate( a, b, line );
+  for( uint32_t i=0; i<line.size(); ++i ) {
+    PutPixelSDL( screen, line[i].x, line[i].y, color );
+  }
+}
+
+//Draw the edges of each polygon in the scene
+void DrawPolygonEdges( const vector<vec4>& vertices, screen* screen ) {
+  int V = vertices.size();
+  // Transform each vertex from 3D world position to 2D image position:
+  vector<ivec2> projectedVertices( V );
+  for( int i=0; i<V; ++i )
+  {
+    VertexShader( vertices[i], projectedVertices[i] );
+  }
+  // Loop over all vertices and draw the edge from it to the next vertex:
+  for( int i=0; i<V; ++i )
+  {
+    int j = (i+1)%V; // The next vertex
+    vec3 color( 1, 1, 1 );
+    DrawLineSDL( screen, projectedVertices[i], projectedVertices[j], color );
+  }
+}
+
+//Determine the position of the projected vertices
 void VertexShader( const vec4& v, ivec2& p ){
   vec4 n = v - cameraPos;
   p.x = (int)((focalLength * (n.x/n.z)) + (SCREEN_WIDTH * 0.5));
   p.y = (int)((focalLength * (n.y/n.z)) + (SCREEN_HEIGHT * 0.5));
 }
 
-//Rotatin about the origin
+//Transform the geometry depending on some translation and rotation vector
 void TransformationMatrix(mat4 Transformation, vec3 angles, vec3 translation){
   float a = angles[0];
   float b = angles[1];
